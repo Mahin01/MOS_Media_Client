1c4d1473bfd4e39b845037f01f28856ee34d8db2
Mahin01 <Ashmahin4@gmail.com>
2023-06-07 20:56:24 +0600

    Navbar Layout Created

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import NavMenu from './Shared/Navbar/NavMenu.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <NavMenu></NavMenu>
   </React.StrictMode>,
 )
