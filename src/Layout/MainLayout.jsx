39b6a5a1ec41d5c3ba1104a51a2ddcbc8c25ab6e
Mahin01 <Ashmahin4@gmail.com>
2023-06-08 15:29:36 +0600

    Main Layout Created

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import NavMenu from '../Shared/Navbar/NavMenu';
+import Footer from '../Shared/Footer/Footer';
+
+const MainLayout = () => {
+    return (
+        <>
+            <NavMenu></NavMenu>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </>
+    );
+};
+
+export default MainLayout;
