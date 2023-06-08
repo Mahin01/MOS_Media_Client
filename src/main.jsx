8752ce0e5ee2d7d9fdae703eb7e2dd9453448af9
Mahin01 <Ashmahin4@gmail.com>
2023-06-08 15:00:33 +0600

    Footer Layout Created & Style Added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import NavMenu from './Shared/Navbar/NavMenu.jsx'
+import Footer from './Shared/Footer/Footer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NavMenu></NavMenu>
+    <Footer></Footer>
   </React.StrictMode>,
 )
