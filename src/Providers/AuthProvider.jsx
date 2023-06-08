568e175713c79da2686a50740425359bf1a17973
Mahin01 <Ashmahin4@gmail.com>
2023-06-08 21:36:27 +0600

    Auth Provider Added

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from 'react';
+import app from '../Configs/Firebase/firebase.config';
+import { getAuth, signInWithEmailAndPassword} from "firebase/auth"
+
+const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+
+    const userSignIn = (email, password) => {
+       return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const authInfo = {
+        user,
+        userSignIn
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
