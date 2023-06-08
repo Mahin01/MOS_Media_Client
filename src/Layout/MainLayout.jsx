d7a1d761cc41203220335f95218a3b9b19f1ada9
Mahin01 <Ashmahin4@gmail.com>
2023-06-08 15:40:04 +0600

    Main Layout routes defined

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -11,5 +11,4 @@ const MainLayout = () => {
         </>
     );
 };
-
 export default MainLayout;
--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -1,20 +1,17 @@
-import React from "react";
-import ReactDOM from 'react-dom/client'
-import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
+import {createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../Pages/Home/Home";
 
-
-  const router = createBrowserRouter([
+ export const router = createBrowserRouter([
     {
-      path: "/",
-      element: <div>Hello world!</div>,
-    },
-  ]);
+        path: "/",
+        element: <MainLayout></MainLayout>,
+        children: [
+          {
+            path: "/",
+            element: <Home></Home>
+          },
+        ]
+    }
+]);
 
-  ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  );
