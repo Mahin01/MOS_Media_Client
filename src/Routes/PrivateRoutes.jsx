5458f77eb63f93f90a28359aa8fdc189d6f38801
Mahin01 <Ashmahin4@gmail.com>
2023-06-10 23:01:14 +0600

    Private Rotue Added

---
+++ src/Routes/PrivateRoutes.jsx
@@ -0,0 +1,23 @@
+import { Children, useContext } from "react";
+import { AuthContext } from "../Providers/AuthProvider";
+import { Navigate } from "react-router-dom";
+
+const PrivateRoutes = ({children}) => {
+
+    const {user, loading} = useContext(AuthContext);
+    
+    if(loading){
+        return <div className="progress" role="progressbar" aria-label="Success example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+            <div className="progress-bar bg-success" style={{ width:"25%" }}></div>
+        </div>
+    }
+
+    if(user?.email){
+        return children;
+    }
+
+    return <Navigate to="/login" replace={true}></Navigate>;
+
+}
+
+export default PrivateRoutes;
