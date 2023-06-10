96a0bbd9a775f67a1eb9f750b92283f875609672
Mahin01 <Ashmahin4@gmail.com>
2023-06-10 22:46:00 +0600

    Active route Implemented

--- src/Pages/Shared/Navbar/NavMenu.jsx
+++ src/Pages/Shared/Navbar/NavMenu.jsx
@@ -1,4 +1,4 @@
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { NavLink, Navigate, useNavigate } from "react-router-dom";
 import "./NavMenu.css";
 import { useContext } from "react";
 import { AuthContext } from "../../../Providers/AuthProvider";
@@ -21,28 +21,17 @@ const NavMenu = () => {
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li>
-                        <a>Parent</a>
-                        <ul className="p-2">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
-                        </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
-                    </ul>
                     </div>
                     <img className="brand-logo" src="logo.png" />
                     <a className="btn btn-ghost normal-case text-xl">Mos Media</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                    <li><Link to={"/"}>Home</Link></li>
+                    <ul className="menu menu-horizontal px-1 dropdown-content">
+                    <li><NavLink className={({ isActive }) => (isActive ? 'active-link' : "")} to={"/"}>Home</NavLink></li>
                     <li>
-                       <Link to={"/instructors"}>Instructors</Link>
+                       <NavLink className={({ isActive }) => (isActive ? 'active-link' : "")} to={"/instructors"}>Instructors</NavLink>
                     </li>
-                    <li><Link to={"/classes"}>Classes</Link></li>
+                    <li><NavLink className={({ isActive }) => (isActive ? 'active-link' : "")} to={"/classes"}>Classes</NavLink></li>
                     </ul>
                 </div>
 
@@ -50,7 +39,7 @@ const NavMenu = () => {
                 <div className="navbar-end">
                 <div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content p-2">
-                        <li><Link to={"/dashboard"}>Dashboard</Link></li>
+                        <li><NavLink className={({ isActive }) => (isActive ? 'active-link' : "")} to={"/dashboard"}>Dashboard</NavLink></li>
                     </ul>
                 </div>
                     <div className="dropdown dropdown-end user-profile">
@@ -74,7 +63,7 @@ const NavMenu = () => {
                     <div className="navbar-end">
                     <ul tabIndex={0} className="menu menu-sm dropdown-content p-2">
                         <ul tabIndex={0} className="menu menu-sm dropdown-content p-2">
-                            <button className="login-btn"><Link to={"/login"}>Login</Link></button>
+                            <button className="login-btn"><NavLink to={"/login"}>Login</NavLink></button>
                         </ul>
                     </ul>
                     </div>
