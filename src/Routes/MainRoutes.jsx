6182c8119684f3d3d7ab53b438118f12d5f30e81
Mahin01 <Ashmahin4@gmail.com>
2023-06-11 00:00:21 +0600

    Dashboard Route path fixed

--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -5,6 +5,8 @@ import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
 import Instructor from "../Pages/Instructors/Instructor";
 import Classes from "../Pages/Classes/Classes";
+import PrivateRoutes from "./PrivateRoutes";
+import Dashboard from "../Layout/Dashboard";
 
  export const router = createBrowserRouter([
     {
@@ -22,6 +24,10 @@ import Classes from "../Pages/Classes/Classes";
           {
             path:"/classes",
             element:<Classes></Classes>
+          },
+          {
+            path:"/dashboard",
+            element: <PrivateRoutes><Dashboard></Dashboard></PrivateRoutes>
           },
           {
             path: "/login",
