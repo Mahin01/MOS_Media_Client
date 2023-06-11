065aa79f3a3b5ec0647fd587af8407c7cc2622a0
Mahin01 <Ashmahin4@gmail.com>
2023-06-11 13:14:13 +0600

    Data fetched from backend

--- src/Pages/Dashboard/SelectedClass/SelectedClass.jsx
+++ src/Pages/Dashboard/SelectedClass/SelectedClass.jsx
@@ -1,5 +1,15 @@
-const SelectedClass = () => {
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../Providers/AuthProvider";
 
+const SelectedClass = () => {
+    const {user} = useContext(AuthContext);
+    const [allSelectedClass, setAllSelectedClasses] = useState();
+    const url = `http://localhost:5000/selected-classes?${user?.email}`;
+    useEffect(() => {
+        fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }, [url]);
     return (
         <div className='w-full px-10'>
            <div className="overflow-x-auto">
