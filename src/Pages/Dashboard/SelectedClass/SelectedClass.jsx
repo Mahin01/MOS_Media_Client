804c5cc7c7f37ad3b6e46bd3c9b7a01b5716c778
Mahin01 <Ashmahin4@gmail.com>
2023-06-12 21:09:24 +0600

    Error fixed on fetching student specific selected classes data

--- src/Pages/Dashboard/SelectedClass/SelectedClass.jsx
+++ src/Pages/Dashboard/SelectedClass/SelectedClass.jsx
@@ -6,7 +6,7 @@ import Swal from "sweetalert2";
 const SelectedClass = () => {
   const { user } = useContext(AuthContext);
   const [allSelectedClasses, setAllSelectedClasses] = useState([]);
-  const url = `http://localhost:5000/selected-classes?${user?.email}`;
+  const url = `http://localhost:5000/selected-classes/${user?.email}`;
 
   useEffect(() => {
     fetch(url)
@@ -27,7 +27,7 @@ const SelectedClass = () => {
       confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
-        fetch(`http://localhost:5000/selected-class/${id}`, {
+        fetch(`http://localhost:5000/student/selected-class/${id}`, {
           method: "DELETE",
         })
           .then(res => res.json())
