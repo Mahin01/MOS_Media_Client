2ac0f0c9ce7351b4e9df4b7f3b5b9647581f3f1c
Mahin01 <Ashmahin4@gmail.com>
2023-06-13 10:11:34 +0600

    Stripe publishable key added

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -3,6 +3,7 @@ import { Elements } from "@stripe/react-stripe-js";
 import { useLoaderData } from "react-router-dom";
 import CheckoutForm from "./CheckoutForm";
 
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 const Payment = () => {  
     const data = useLoaderData();
     const {price} = data;
