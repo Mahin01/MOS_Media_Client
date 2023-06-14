1cd19e45be85debc8a8c0aab8dbb102e55e142f8
Mahin01 <Ashmahin4@gmail.com>
2023-06-14 21:16:21 +0600

    Payment method added

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,21 +1,22 @@
+import React from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-import { useLoaderData } from "react-router-dom";
-import CheckOutForm from "./CheckOutForm";
+import { useLoaderData} from "react-router-dom";
+import CheckoutForm from "./CheckoutFrom";
 
 const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
-const Payment = () => {  
-    const data = useLoaderData();
-    const {price} = data;
 
-    return (
-        <div>
-            <h2 className="text-3xl text-center my-5"> Make Payment</h2>
-            <Elements stripe={stripePromise}>
-                <CheckOutForm data={data} price={price}></CheckOutForm>
-            </Elements>
-        </div>
-    );
+const Payment = () => {
+  const itemData = useLoaderData();
+
+  return (
+    <div>
+      <h2 className="text-3xl text-center my-5">Make Payment</h2>
+      <Elements stripe={stripePromise}>
+        <CheckoutForm itemData={itemData}></CheckoutForm>
+      </Elements>
+    </div>
+  );
 };
 
 export default Payment;
